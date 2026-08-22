6c03eb4f91e7f311bab20c57fa5df860edc2d7f4
kawsar117 <kawsar117@gmail.com>
2026-08-22 17:46:15 +0600

    project>apps.py configured

--- Assignment Module - 07/portfolio_project/projects/apps.py
+++ Assignment Module - 07/portfolio_project/projects/apps.py
@@ -1,5 +1,8 @@
+
+# explanation: This file defines the configuration for the 'projects' app in a Django project. It specifies the default auto field type and the name of the app.
+
 from django.apps import AppConfig
 
-
 class ProjectsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'projects'
