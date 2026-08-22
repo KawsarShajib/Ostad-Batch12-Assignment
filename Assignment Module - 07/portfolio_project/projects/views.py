6286bea37d65c0d4593a94f38689f437eae8c1f2
kawsar117 <kawsar117@gmail.com>
2026-08-22 17:35:47 +0600

    project>views.py configured

--- Assignment Module - 07/portfolio_project/projects/views.py
+++ Assignment Module - 07/portfolio_project/projects/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'projects/home.html')
+
+def about(request):
+    return render(request, 'projects/about.html')
+
+def projects_list(request):
+    projects = Project.objects.all().order_by('-created_at')
+    return render(request, 'projects/projects.html', {'projects': projects})
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'projects/project_detail.html', {'project': project})
